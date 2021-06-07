5cba8c9a86f5e457362b06454b24059fa1617fed
Freddie Gilbraith <freddie.gilbraith@littlebonsai.co.uk>
2021-06-07 14:46:51 +0100

    open the repo, from anywhere in the repo

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,70 @@
-use uuid::Uuid;
+mod issue {
+    use uuid::Uuid;
+    pub struct Id(u64);
 
-struct RootTishIssue {
-    id: u64,
-    uuid: Uuid;
-    status: Status;
+    pub struct RootIssue {
+        id: Id,
+        uuid: Uuid,
+        status: Status,
+        reporter: Author,
 
-    title: String;
-    description: String;
+        title: String,
+        description: String,
 
-                 comments: Vec<TishIssueComment>;
+        comments: Vec<IssueComment>,
+    }
+
+    pub struct Status {
+        resolved: bool,
+        name: String,
+    }
+
+    pub struct IssueComment {
+        text: String,
+        author: Author,
+    }
+
+    pub struct Author {
+        name: String,
+        email: String,
+    }
 }
 
-struct TishIssueComment {
-    text: String;
-          author: TishAuthor;
+mod project {
+    use super::issue;
+
+    struct Config {
+        project: ProjectConfig,
+        statuses: Vec<issue::Status>,
+    }
+
+    ProjectConfig {
+        name: String,
+        key: String,
+    }
 }
 
-struct TishAuthor{
-    name: String;
-          email: String;
+pub mod command {
+    use clap::Clap;
+
+    #[derive(Clap, Debug)]
+    pub struct Root {
+        #[clap(subcommand)]
+        command: Command,
+    }
+
+    #[derive(Clap, Debug)]
+    pub enum Command {
+        Create(CreateOpts),
+    }
+
+    #[derive(Clap, Debug)]
+    pub struct CreateOpts {
+        #[clap(short)]
+        title: String,
+    }
+
+    pub fn cli_parse() -> Root {
+        Root::parse()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use git2::Repository;
+use std::path::{Path, PathBuf};
+use tish::command;
+
+fn recursive_get_repo(maybe_dir: Option<&Path>) -> Repository {
+    let dir = maybe_dir.expect("could not find git repo in this directory or parents");
+
+    match Repository::open(dir) {
+        Ok(repo) => repo,
+        Err(e) => recursive_get_repo(dir.parent()),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let repo = recursive_get_repo(Some(&std::env::current_dir().unwrap()));
+
+    let root_dir = repo
+        .workdir()
+        .expect("could not open repo working directory");
+
+    let config_file_path = root_dir.join("tish.toml");
+
+    dbg!(&config_file_path);
+
+    let tish_command = command::cli_parse();
+
+    dbg!(tish_command);
 }
